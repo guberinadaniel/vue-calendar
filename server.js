a1609739a952595ef951d910347905b58324ff3c
daniel <guberinadaniel@gmail.com>
2018-03-14 15:32:19 +0100

    last commit.

--- server.js
+++ server.js
@@ -5,13 +5,40 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
+const moment = require('moment-timezone');
+moment.tz.setDefault('UTC');
+const serialize = require('serialize-javascript');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+let events = [
+  { description: 'Random event 1', date: moment('2018-03-08', 'YYYY-MM-DD')},
+  { description: 'Random event 2', date: moment('2018-03-02', 'YYYY-MM-DD')},
+  { description: 'Random event 3', date: moment('2018-04-14', 'YYYY-MM-DD')}
+];
+
+let renderer;
+
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
-  res.send(template);
+  let contentMarker = '<!--APP-->';
+  if (renderer) {
+    renderer.renderToString({ events }, (err, html) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(template.replace(contentMarker, `<script>var __INITIAL_STATE__ = ${ serialize(events) }</script>\n${html}`));
+      }
+    });
+    } else {
+      res.send('<p>Awaiting compilation...</p>');
+    }
+});
 
+app.use(require('body-parser').json());
+app.post('/add_event', (req, res) => {
+  events.push(req.body);
+  res.sendStatus(200);
 });
 
 const server = http.createServer(app);
@@ -20,6 +47,10 @@ if (process.env.NODE_ENV === 'development') {
   const reload = require('reload');
   const reloadServer = reload(server, app);
   require('./webpack-dev-middleware').init(app);
+  require('./webpack-server-compiler').init(function (bundle) {
+    renderer = require('vue-server-renderer').createBundleRenderer(bundle);
+  });
+
 }
 
 server.listen(process.env.PORT, function () {
